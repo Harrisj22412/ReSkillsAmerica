5d02122a11d12141874d8c9b9330eb7b65dd910d
Jalen Harris <Harrisj22412@hocking.edu>
2021-04-11 17:00:58 -0400

    Add files via upload

--- MockATMProject.py
+++ MockATMProject.py
@@ -1,33 +1,78 @@
 #\n = drops to the next line
 
+#!!Please make sure you use the same email address through out this program or you may receive an error message. Thanks!
+
 name = input('What is your name? \n')
-allowedUsers = ['Jalen', 'Mike', 'Tony', 'Lisa']
+email = input('Please enter your email address. \n')
+allowedUsers = ['Jay', 'Mike', 'Tony', 'Lisa']
 allowedPassword = ['PasswordJalen', 'PasswordMike', 'PasswordTony', 'PasswordLisa']
+people = {'Tom':730, 'Brad':650, 'Steve':710}
+balance = 500
+counter = 3
+
 import random
 from datetime import date
 today = date.today()
 
+#current time
+import time
+current = time.localtime()
+current_time = time.strftime("%H:%M")
+print(current_time)
+
+
 # dd/mm/YY
-d1 = today.strftime("%d/%m/%Y")
+d1 = today.strftime("%B %d, %Y")
 print( d1)
 
 from datetime import datetime
 now = datetime.now()
-print(now)
+#print(now)
 
 database = {}
 
 def complaint(selectOption):
     print('You selected %s' % selectOption)
     complaint = ''
-    complaint = input('Please enter your complaint? ' )
-    input('You entered ' + complaint + ' is this correct? ' )
-    if(complaint):
+    complaint = input('Please enter your complaint? \n' )
+    complaint_response = input('You entered ' + complaint + '. Is this correct? \n' )
+    if(complaint_response == 'yes'):
         print('We do apologize for the inconvenience, our customercare team will be in contact with you within the next 24 hours. Have a great day, and thanks for choosing Chase Bank ')
+        print(d1)
+        print(current_time)
         main()
-            
+
+        
+
+def update_balance():
+    global balance
+    print('This is your current balance')
+    print(balance)
+    print(current_time)
+    print(d1)
+    main()
+
+def lockedOut():
+    global counter
+    while counter > 1:
+        print('Please try agian')
+        counter = counter - 1
+
+    else:
+        print('You have been locked out')
+    
+
+def creditCard(people):
+    for member in people:
+        if people[member] > 700:
+            print('Approved', member, people[member])
+                
+        else:
+            print("Decline", member, people[member])
+
 
 def Deposit(selectOption):
+    global balance
     print('You selected %s' % selectOption)
     print('Please insert cash.')
     cash = input('How much cash did you enter: ')
@@ -35,11 +80,16 @@ def Deposit(selectOption):
                 
     correct_cash = input('')
     if(correct_cash == 'yes'):
+        cash = int(cash)
+        balance = cash + balance
+        new_balance = str(balance)
+        print('This is your new balance ' + new_balance)
+        print(d1)
         print('Have a great day, and thanks for choosing Chase Bank!')
         main()
     elif(correct_cash == 'no'):
         print('Please renter the cash.')
-        print('Please insert cash.')
+        
         cash = input('How much cash did you enter: ')
         print(cash + " is the amount that is displayed on the screen correct?")
         correct_cash = input('')
@@ -51,22 +101,33 @@ def Deposit(selectOption):
         
 
 def Withdrawl(selectOption):
+    global balance
     print('You selected %s' % selectOption)
     print(name + ' how much would you like to withdrawl today?')
     amount = input('')
+    new_amount = int(amount)
     print('You selected ' + amount + ' is this correct?')
-    first_respone = 'yes'
+    
     first_response = input('')
     if(first_response =='yes'):
+        balance =  balance - new_amount
+        print('This is your new balance.')
+        print(balance)
+        print(current_time)
+        print(d1)
         print('Have a great day and thanks for choosing Chase Bank!')
         returnres = input('Would you like to return to the main menu?')
         if(returnres == 'yes'):
             main()
         elif(returnres =='no'):
             print('Please renter the amount that you need.')                                
-            
+
         
 def main():
+    counter = 3
+    global name
+    global password
+    global balance
     allowedUsers = ['Jalen', 'Mike', 'Tony', 'Lisa']
     allowedPassword = ['PasswordJalen', 'PasswordMike', 'PasswordTony', 'PasswordLisa']
     if(name in allowedUsers):
@@ -76,31 +137,72 @@ def main():
     if(password == allowedPassword[userId]):
         print('Welcome  %s' %  name)
         print('These are the available options:' )
-        print('1.Register') 
+        print('1.Register')
+        print('2. Change Password') 
+        print('3. Update Address')
         print('4. Withdrawl')
         print('5. Deposit')
         print('6. Complaint')
+        print('7. Balance')
 
-        selectOption = int(input('Please select an option:' ))
-        print(d1, now)
-        userInput = True 
+        selectOption = int(input('Please select an option: \n' ))
+        print(current_time)
+        print(d1 )
         print(selectOption)
         if(selectOption == 1):
             register()
+        elif(selectOption == 2):
+            changePassword()
+        elif(selectOption == 3):
+            updateAddress()
         elif(selectOption == 4):
             Withdrawl(selectOption) 
         elif(selectOption == 5):
             Deposit(selectOption)
         elif(selectOption == 6):
             complaint(selectOption)
+        elif(selectOption == 7):
+            update_balance()
         elif(selectOption != 4, selectOption != 5, selectOption != 6, selectOption): 
             print('Invalid option selected, please try again.')
         
     elif(password  not in allowedPassword):
-        print('Password incorrect, please try again.')
-
+        lockedOut()
+        
     else:
-        print('Name is not found, please try again.') 
+        print('Name is not found, please try again or select change password option')
+         
+
+def changePassword():
+    
+    secure_code_generator = random.randrange(1111, 9999)
+    print(secure_code_generator)
+    verify = input("Can you please verify your email address please? \n")
+    if (email == verify):
+        
+        print("A code was sent to your email address")
+        usercode = input('Please enter code: \n')
+
+        if(secure_code_generator == int(usercode)):
+            print('Your code was accepted and you may now login')
+    main()
+        #Just a demo for verification and generating a code. 
+        
+
+
+def updateAddress():
+    verify_password = input('Please verify your password \n')
+    if(verify_password == allowedPassword[0]):
+        print('Password was correct!')
+        new_address = input('Please enter new address \n')
+        is_correct = input('The new address you have entered is' +  new_address + 'is this correct \n')
+        if(is_correct == 'yes'):
+            print('Your ' + new_address + 'has been updated and you may go back to the main menu.')
+            main()
+        elif(is_correct == 'no'):
+            updateAddress()
+
+
 
 def depositOperation():
     print('Deposit Operation')
@@ -115,26 +217,6 @@ def bankOperation(user):
     print('Welcome %s %s' % (user[0], user[1]))
 
 
-   
-    selected_option = int(input("What would you like to do? (1) Deposit (2) Withdrawl(3) Logout (4) Exit \n" ))
-
-    if(selected_option == 1):
-            
-        depositOperation()
-    elif(selected_option == 2):
-            
-        withdrawlOperation()
-    elif(selected_option == 3):
-            
-        logout()
-    elif(selected_option == 4):
-            
-        exit()
-    else:
-            
-        print("Invalid option selected")
-        bankOperation(user)
-
 
 def login():
     print('****** Login*********')
@@ -148,8 +230,10 @@ def login():
                 bankOperation(userDetails)
                     
         
-        print('Invalid account number or password.')
-        login()
+            else:
+                print('Invalid account number or password.')
+        main()
+        #login()
 
 def gernerateAccountNumber():
     
@@ -168,7 +252,6 @@ def register():
     database[accountNumber] = [first_name, last_name, email, password]
 
     print('Your account has been created.')
-    print("== ==== ===== ==== ===")
     print("Your account number is %d" % accountNumber)
     print("Make sure you keep it safe")
 
