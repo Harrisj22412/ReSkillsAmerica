58e41b2d4f143ce8881b711ff6e6a7d20a51a99f
Jalen Harris <Harrisj22412@hocking.edu>
2021-04-25 09:29:25 -0400

    Add files via upload
    I uncommented the expenses() function. Still working out some errors for that function. I also will apply one function to my Food Class which inherits from the Parent Class Budget.  That function will print out the list of items following an amount for each item. So far I only made two objects which our my_car and jalen_budget.  Run the uncommented print statements and function calls one at a  time located at the bottom to run the program.

---
+++ BudgetProject.py
@@ -0,0 +1,120 @@
+global balance
+
+global amount
+
+
+class Budget:
+    #amount = []
+
+    def __init__(self, category, amount):
+        self.category = category
+        self.amount = amount
+        self.balance = 300
+        
+
+    def transfer(self):
+        print('Transfer was successfully made for your payment today.')
+        #genereate and print confirmation number
+
+    #def expenses(self, amount):
+        #self.amount = str(amount)
+        #print('These are the expenses for ' + '' +  self.category + '' +  self.amount)
+
+    def deposit(self, amount, balance):
+        
+        balance += amount
+        deposit_response = input('You just got paid, would you like to check your balance?: \n')
+        if deposit_response  == 'yes':
+            print(balance)
+        elif deposit_response == 'no':
+            print("Thanks for choosing Chase Bank, have a great day.")
+         
+        else: print('Invalid choice selection please try again.')
+
+    def withdrawal(self, balance, amount):
+        amount = int(amount)
+        request = input('How much would you like to withdraw today?: \n')
+        balance -= amount
+        request = balance
+        request = int()
+        if request <= balance :
+            request -= balance
+            check = input("Would you now like to check your balance?: \n")
+            if check == 'yes':
+                print(balance)
+                print("Thanks for choosing Chase Bank, have a great day!")
+            elif check == 'no':
+                print("Thanks for choosing Chase Bank, have a great day!")
+
+        elif request > balance:
+            print('Sorry, this account has insufficient funds.')
+            transfer_opt = input('Would you like to  transfer funds to this account? \n')
+            if transfer_opt == 'yes':
+                print(balance)
+                
+            elif transfer_opt == 'no':
+                print("Thanks for choosing Chase Bank, have a great day!")
+
+        else: print('Invalid selection option please try again.')
+        #withdrawal(amount)
+
+    def check_balance(self, balance):
+        print(balance)
+    
+class Food(Budget):
+    def __init__(self, item, amount):
+        super().__init__(self, amount)
+        self.item = item
+        self.amount = amount
+        
+        
+
+class Clothing(Budget):
+    def __init__(self, item, amount):
+        super().__init__(self, amount)
+        self.item = item
+        self.amount = str(amount)
+
+    def clothing_expenses(self):
+        
+        print('These are the expenses for' + ' ' + self.item + ' '+ self.amount)
+        
+
+class Car(Budget):
+    def __init__(self, model, year, amount):
+        super().__init__(self, amount)
+        
+        self.model = model
+        self.year = year
+        self.amount = amount
+
+    def car_details(self, amount):
+        return '{} {} {}'.format(self.model, self.year, self.amount)
+        print(self.model, self.year, self.amount)
+        
+
+#j is an object created from the class Car
+my_car = Car('Telsa', 2021 , 550)
+#print(my_car.car_details(550))
+
+jalen_budget = Budget('Entertainment ', 600)
+
+#print(jalen_budget.expenses(300))
+
+
+#jalen_budget.withdrawal(600)
+
+#jalen_budget.deposit(500, 300)
+
+#t is an object created from the class Car
+clothing_category = Clothing('Nike', '300')
+#print(clothing_category.clothing_expenses())
+
+
+
+
+
+#print(Budget(category_1))
+
+#print(help(Budget))
+
